866a36b6f2d41ff48c74eb7884b1c1fc0d71984d
iamrobinhood12345 <swordsshadow@Williams-MacBook-Pro.local>
2016-12-07 16:43:28 -0800

    added make_tri_dict function that makes dict of key pair to word list values.

--- src/trigrams.py
+++ src/trigrams.py
@@ -1,12 +1,14 @@
 """This module implements the trigrams algorithm for a given text file."""
 
+
 from __future__ import print_function
 import random
 import io
 import sys
 import re
 
-TRI_DICT = {}
+
+# TRI_DICT = {}
 
 
 def open_file(path):
@@ -18,7 +20,8 @@ def open_file(path):
 
 
 def format_text(text):
-    """Format string return from file to remove punctuation, newlines, and capital letters."""
+    """Format string return from file to remove punctuation, 
+    newlines, and capital letters."""
     text = text.replace('--', ' ')  #special case
     text = text.replace('\n', ' ')
     text = re.sub('[^ a-zA-Z]', '', text)
@@ -26,11 +29,25 @@ def format_text(text):
     return text.lower()
 
 
+def make_tri_dict(word_list):
+    """Make key value pairs of pairs of words to a list of words from a
+    list of words from the text in order."""
+    tri_dict = {}
+    key_pair = ''
+    next_word = ''
+    for i in range(len(word_list) - 3):
+        key_pair = word_list[i] + ' ' + word_list[i + 1]
+        next_word = word_list[i + 2]
+        if key_pair in tri_dict:
+            tri_dict[key_pair] += next_word
+        else:
+            tri_dict[key_pair] = next_word
+    return tri_dict
+
+
 def main(path, num_words):
     """This function implements the core trigrams algorithm"""
-    format_text(open_file(path))
-
-
+    make_tri_dict(format_text(open_file(path)).split())
 
 
 if __name__ == '__main__':
